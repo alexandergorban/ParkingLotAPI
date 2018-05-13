721170d66bb7213ba32480ed57b8ab139cd97ed1
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-05-13 19:40:56 +0300

    Use api services (fixes #17)

--- ParkingLotWebAPI/Startup.cs
+++ ParkingLotWebAPI/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using ParkingLotCore;
+using ParkingLotWebAPI.Services;
 
 namespace ParkingLotWebAPI
 {
@@ -16,14 +18,26 @@ namespace ParkingLotWebAPI
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
+            CarsService = new CarsService();
+            ParkingService = new ParkingService();
+            TransactionsService = new TransactionsService();
         }
 
         public IConfiguration Configuration { get; }
+
+        public CarsService CarsService { get; private set; }
+        public ParkingService ParkingService { get; private set; }
+        public TransactionsService TransactionsService { get; private set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddScoped<CarsService>();
+            services.AddScoped<ParkingService>();
+            services.AddScoped<TransactionsService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
