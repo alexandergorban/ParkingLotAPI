84810b085b152a967c843a10a16b9b6aaf942f8d
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-05-13 05:17:50 +0300

    Renamed services

--- ParkingLotWebAPI/Services/ParkingService.cs
+++ ParkingLotWebAPI/Services/ParkingService.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ParkingLotCore;
 
 namespace ParkingLotWebAPI.Services
 {
     public class ParkingService
     {
+        public CoreApp CoreApp { get; private set; }
+
+        public ParkingService()
+        {
+            CoreApp = CoreApp.Instance;
+        }
+
+        //Number of free places (GET)
+        public string GetNumberFreePlaces()
+        {
+            return CoreApp.Parking.GetNumberAvailableParkingSpaces().ToString();
+        }
+
+        //Number of occupied places (GET)
+        public string GetNumberOccupiedPlaces()
+        {
+            return CoreApp.Parking.GetNumberAvailableParkingSpaces().ToString();
+        }
+
+        //General Income (GET)
+        public string GetGeneralIncome()
+        {
+            return CoreApp.Parking.GetEarnedMoney().ToString();
+        }
     }
 }
--- ParkingLotWebAPI/Services/TransactionsService.cs
+++ ParkingLotWebAPI/Services/TransactionsService.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ParkingLotCore;
 
 namespace ParkingLotWebAPI.Services
 {
     public class TransactionsService
     {
+        public CoreApp CoreApp { get; private set; }
+
+        public TransactionsService()
+        {
+            CoreApp = CoreApp.Instance;
+        }
+
+        //View Transactions.log (GET)
+
+        //Last minute transaction (GET)
+
+        //Transaction for the last minute on one particular machine(GET)
+
+        //Fill the machine balance (PUT)
     }
 }
